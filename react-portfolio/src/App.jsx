9a8ce6672c19dc3476e2a54de0adabc4251e0903
krosengr4 <rosenkev4@gmail.com>
2024-01-08 08:33:27 -0600

    got rid of unnecessary files

--- react-portfolio/src/App.jsx
+++ react-portfolio/src/App.jsx
@@ -19,7 +19,11 @@ function App() {
       <h1>Vite + React</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+          Increase count 
+        </button>
+       <h3> Count is: {count} </h3> 
+        <button onClick={() => setCount((count) => count - 1)}>
+          Decrease count 
         </button>
         <p>
           Edit <code>src/App.jsx</code> and save to test HMR
