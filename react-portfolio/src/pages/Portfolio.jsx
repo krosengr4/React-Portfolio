b102f4c6d996881b83d57afb3f38aae7eca6deaa
krosengr4 <rosenkev4@gmail.com>
2024-01-11 23:07:26 -0600

    Imported photos and semi styled. 4 btm pictures neeed work

--- react-portfolio/src/pages/Portfolio.jsx
+++ react-portfolio/src/pages/Portfolio.jsx
@@ -5,15 +5,52 @@ export default function Portfolio() {
     return (
       <div className="main-div">
 
-        <div className="top-two">
-          <button className="picture-btns">
-            <a href="/Rooster-Tail.png"></a>
-          </button>
-        </div>
         <h1 className="page-title">Portfolio: </h1>
         <p className="aboutme-content">
             This is where your portfolio page will go. 
         </p>
+        <div className="top-two">
+        <a class="top-project" href="https://krosengr4.github.io/Weather-Dashboard/" alt="Image of My Project Website">
+          {/* <image class="big-image" src="./assets/images/Weather-Dashboard.png" alt="Image of Horiseon Accessibility website"> </image> */}
+            <img src= "./public/Rooster-Tail.png" className="picture-btn" alt="Vite logo" href="./public/Rooster-Tail.png"/>
+              <div class="overlay">
+                  {/* <h3 class="text-h3">Weather Dashboard</h3> */}
+                  {/* <div class="text"> Click me to go to the Website!</div> */}
+              </div>
+        </a>
+        <a class="btm-project" href="https://krosengr4.github.io/Weather-Dashboard/" alt="Image of My Project Website">
+          {/* <image class="big-image" src="./assets/images/Weather-Dashboard.png" alt="Image of Horiseon Accessibility website"> </image> */}
+            <img src= "./public/Rooster-Tail.png" className="btm-picture-btn" alt="Vite logo" href="./public/Rooster-Tail.png"/>
+              <div class="overlay">
+                  {/* <h3 class="text-h3">Weather Dashboard</h3> */}
+                  {/* <div class="text"> Click me to go to the Website!</div> */}
+              </div>
+        </a>
+        <a class="btm-project" href="https://krosengr4.github.io/Weather-Dashboard/" alt="Image of My Project Website">
+          {/* <image class="big-image" src="./assets/images/Weather-Dashboard.png" alt="Image of Horiseon Accessibility website"> </image> */}
+            <img src= "./public/Rooster-Tail.png" className="btm-picture-btn" alt="Vite logo" href="./public/Rooster-Tail.png"/>
+              <div class="overlay">
+                  {/* <h3 class="text-h3">Weather Dashboard</h3> */}
+                  {/* <div class="text"> Click me to go to the Website!</div> */}
+              </div>
+        </a>
+        <a class="btm-project" href="https://krosengr4.github.io/Weather-Dashboard/" alt="Image of My Project Website">
+          {/* <image class="big-image" src="./assets/images/Weather-Dashboard.png" alt="Image of Horiseon Accessibility website"> </image> */}
+            <img src= "./public/Rooster-Tail.png" className="btm-picture-btn" alt="Vite logo" href="./public/Rooster-Tail.png"/>
+              <div class="overlay">
+                  {/* <h3 class="text-h3">Weather Dashboard</h3> */}
+                  {/* <div class="text"> Click me to go to the Website!</div> */}
+              </div>
+        </a>
+        <a class="btm-project" href="https://krosengr4.github.io/Weather-Dashboard/" alt="Image of My Project Website">
+          {/* <image class="big-image" src="./assets/images/Weather-Dashboard.png" alt="Image of Horiseon Accessibility website"> </image> */}
+            <img src= "./public/Rooster-Tail.png" className="btm-picture-btn" alt="Vite logo" href="./public/Rooster-Tail.png"/>
+              <div class="overlay">
+                  {/* <h3 class="text-h3">Weather Dashboard</h3> */}
+                  {/* <div class="text"> Click me to go to the Website!</div> */}
+              </div>
+        </a>
+        </div>
       </div>
     );
   }
