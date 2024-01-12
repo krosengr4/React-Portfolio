0eda29393f3fded07aedd758c77df82f5387ca4a
krosengr4 <rosenkev4@gmail.com>
2024-01-12 16:39:26 -0600

    Added pic of myself and styled Added to about me page, styled in app.css

--- react-portfolio/src/pages/AboutMe.jsx
+++ react-portfolio/src/pages/AboutMe.jsx
@@ -12,12 +12,14 @@ export default function AboutMe() {
       <div className="main-div">
         <h1 className="page-title">About Me </h1>
         <hr></hr>
+
+        <img src="./public/Pic-of-me.jpg" className="pic-of-me" alt="Picture of Kevin Rosengren"></img>
         <p className="aboutme-content">
-                        Howdy! I am Kevin Rosengren, a 23 year old Texan native who loves technology and programming. Both of my 
-                        parents were progammers at IBM for a long time, so the curiosity and passion for technology began at a young age. I am now
-                        enrolled in the University of Texas web developement bootcamp and have really enjoyed and immersed myself in the course. I 
-                        love to learn and acquire new skills and have other hobbies which include baseball, hockey, learning and playing musical instruments,
-                        and getting outdoors whenever I can. I am curious, excited, and determined to make web developement and programming a career.
+            Howdy! I am Kevin Rosengren, a 23 year old Texan native who loves technology and programming. Both of my 
+            parents were progammers at IBM for a long time, so the curiosity and passion for technology began at a young age. I am now
+            enrolled in the University of Texas web developement bootcamp and have really enjoyed and immersed myself in the course. I 
+            love to learn and acquire new skills and have other hobbies which include baseball, hockey, learning and playing musical instruments,
+            and getting outdoors whenever I can. I am curious, excited, and determined to make web developement and programming a career.
         </p>
 
         <div className="like-dislikeBtn">
