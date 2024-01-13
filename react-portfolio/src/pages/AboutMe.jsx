de98c9fc52afe6af23837ff02f9dacf20445b937
krosengr4 <rosenkev4@gmail.com>
2024-01-13 08:35:41 -0600

    Update colors, remove ./public on images. Public is in root directory so only needed /image.png

--- react-portfolio/src/pages/AboutMe.jsx
+++ react-portfolio/src/pages/AboutMe.jsx
@@ -13,7 +13,7 @@ export default function AboutMe() {
         <h1 className="page-title">About Me </h1>
         <hr></hr>
 
-        <img src="./public/Pic-of-me.jpg" className="pic-of-me" alt="Picture of Kevin Rosengren"></img>
+        <img src="/Pic-of-me.jpg" className="pic-of-me" alt="Picture of Kevin Rosengren"></img>
         <p className="aboutme-content">
             Howdy! I am Kevin Rosengren, a 23 year old Texan native who loves technology and programming. Both of my 
             parents were progammers at IBM for a long time, so the curiosity and passion for technology began at a young age. I am now
--- react-portfolio/src/pages/Portfolio.jsx
+++ react-portfolio/src/pages/Portfolio.jsx
@@ -9,32 +9,32 @@ export default function Portfolio() {
         <hr></hr>
     <section className='projects-container'>
         <a className='top-project' href='https://lmansilla92.github.io/rooster-tail/'>
-            <img className='project-img' src='./public/Rooster-Tail.png' alt='Clickable image of my project'></img>
+            <img className='project-img' src='/Rooster-Tail.png' alt='Clickable image of my project'></img>
             <div className='overlay'>
                 <h3 className='big-project-label'>Rooster Tail</h3>
             </div>
         </a>
 
         <a className='lower-project' href='https://krosengr4.github.io/Weather-Dashboard/'>
-            <img className='project-img' src='./public/Weather-Dashboard.png' alt='Clickable image of my project'></img>
+            <img className='project-img' src='/Weather-Dashboard.png' alt='Clickable image of my project'></img>
             <div className='overlay2'>
                 <h3 className='small-project-label'>Weather Dashboard</h3>
             </div>
         </a>
         <a className='lower-project' href='https://krosengr4.github.io/Coding-Quiz/'>
-            <img className='project-img' src='./public/Code-Quiz.png' alt='Clickable image of my project'></img>
+            <img className='project-img' src='/Code-Quiz.png' alt='Clickable image of my project'></img>
             <div className='overlay3'>
                 <h3 className='small-project-label'>Coding Quiz</h3>
             </div>
         </a>
         <a className='lower-project' href='https://krosengr4.github.io/WorkDayScheduler/'>
-            <img className='project-img' src='./public/Workday-scheduler.png' alt='Clickable image of my project'></img>
+            <img className='project-img' src='/Workday-scheduler.png' alt='Clickable image of my project'></img>
             <div className='overlay4'>
                 <h3 className='small-project-label'>Workday Scheduler</h3>
             </div>
         </a>
         <a className='lower-project' href='https://krosengr4.github.io/Password-Generator/'>
-            <img className='project-img' src='./public/Password-Generator.png' alt='Clickable image of my project'></img>
+            <img className='project-img' src='/Password-Generator.png' alt='Clickable image of my project'></img>
             <div className='overlay5'>
                 <h3 className='small-project-label'>Random Password Generator</h3>
             </div>
